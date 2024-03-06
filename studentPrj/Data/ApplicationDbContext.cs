bebcb8b1346d856ba335dd400fafcba62d7e9961
Mshazic <158284545+C2ngo@users.noreply.github.com>
2024-03-06 15:42:03 +0200

    Application dbcontext class added

---
+++ studentPrj/Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+namespace studentPrj.Data
+{
+	public class ApplicationDbContext: DbContext
+	{
+		public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options)
+		{
+		}
+	}
+}
+
