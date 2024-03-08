0811e00505ac1d5b72285599b1ac09d9e24d648b
Mshazic <158284545+C2ngo@users.noreply.github.com>
2024-03-08 12:26:32 +0200

    creating to db done

--- studentPrj/Controllers/StudentsController.cs
+++ studentPrj/Controllers/StudentsController.cs
@@ -3,6 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using studentPrj.Data;
+using studentPrj.Models;
+using studentPrj.Models.Entities;
+
+
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -10,12 +15,40 @@ namespace studentPrj.Controllers
 {
     public class StudentsController : Controller
     {
+        private readonly ApplicationDbContext dbContext;
+        public StudentsController(ApplicationDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
         // GET: /<controller>/
         [HttpGet]
         public IActionResult Add()
         {
             return View();
         }
+        [HttpPost]
+        public async Task<IActionResult> Add(AddStudentViewModelClass viewModel)
+        {
+            var student = new Student
+            {
+                Name = viewModel.Name,
+                Email = viewModel.Email,
+                Phone = viewModel.Phone,
+                Subscribe = viewModel.Subscribe
+
+            };
+
+            await dbContext.Students.AddAsync(student);
+            await dbContext.SaveChangesAsync();
+
+            return View();
+        }
+
+
+       
     }
+
+
 }
 
