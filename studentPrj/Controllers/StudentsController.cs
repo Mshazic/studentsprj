11c6e9f968a1dbbffe1a97cd6861d37cc3a37331
Mshazic <158284545+C2ngo@users.noreply.github.com>
2024-03-08 14:29:27 +0200

    delete wip

--- studentPrj/Controllers/StudentsController.cs
+++ studentPrj/Controllers/StudentsController.cs
@@ -81,11 +81,11 @@ namespace studentPrj.Controllers
         [HttpPost]
         public async Task<IActionResult> Delete(Student viewModel)
         {
-            var student = await dbContext.Students.FindAsync(viewModel.Id);
+            var student = await dbContext.Students.AsNoTracking().FirstOrDefaultAsync(x => x.Id == viewModel.Id);
             if (student is not null)
             {
                 dbContext.Students.Remove(viewModel);
-                await dbContext.SaveChanges();
+                await dbContext.SaveChangesAsync();
             }
             return RedirectToAction("List", "students");
 
